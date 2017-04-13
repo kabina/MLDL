e08695b6ec1a23be0756507e89b5331813038ca9
An ChangSun <nheo.an@gmail.com>
2017-04-13 18:08:15 +0900

    다듬음 - crawl_multiProcess

--- SPIDER/crawl/crawl_multiProcess.py
+++ SPIDER/crawl/crawl_multiProcess.py
@@ -1,3 +1,8 @@
+'''
+멀티 프로세스 기반의 크롤링(naver 뉴스)
+크롤링 후 형태소분석기를 이용한 단어 빈도 계산
+by An ChangSun
+'''
 #-*- encoding: utf-8 -*-
 import pymysql as mdb
 import sys
@@ -6,6 +11,8 @@ import datetime, time
 import imp
 import re
 import urllib3
+from konlpy.tag import Kkma
+from konlpy.utils import pprint
 from multiprocessing import Process, Queue
 
 from bs4 import BeautifulSoup
@@ -30,8 +37,11 @@ def crawl(date, counter):
 		for links in soup.findAll('div', {'class':'tmp small2 ',}):
 			link = links.find('a')
 			href = link.get('href')
-			urldetail = 'http://news.naver.com'+str(href)
 			title = link.get('title')
+			if(title is None) :
+				continue
+			urldetail = 'http://news.naver.com'+str(href)
+
 
 			r = http.request('GET', urldetail)
 			plain_text = r.data
@@ -47,13 +57,12 @@ def crawl(date, counter):
 			#news['title'] = re.sub('[^가-힝\\s]', '', str(title).strip())
 			#news['href'] = re.sub('[^가-힝\\s]', '', str(href).strip())
 			#news['body'] = re.sub('[^가-힝\\s]', '', str(body).strip())
-
-			#news['title'] = re.sub('[ ^ \.\, \?\!a - zA - Z0 - 9\u3131 -\u3163\uac00 -\ud7a3]+', '', str(title).strip())
-			#news['href'] = re.sub('[ ^ \.\, \?\!a - zA - Z0 - 9\u3131 -\u3163\uac00 -\ud7a3]+', '', str(href).strip())
-			#news['body'] = re.sub('[ ^ \.\, \?\!a - zA - Z0 - 9가-힝]+', '', str(body).strip())
-			news['title'] = re.sub('[ ^\.\,\?\!a-zA-Z0-9가-힝]+', '', str(title).strip())
-			news['href'] = re.sub('[ ^\.\,\?\!a-zA-Z0-9가-힝]+', '', str(href).strip())
-			news['body'] = re.sub('[ ^\.\,\?\!a-zA-Z0-9가-힝]+', '', str(body).strip())
+			#news['title'] = re.sub(str(title).strip())
+			#news['href'] = str(href).strip()
+			#news['body'] = re.sub(str(body).strip())
+			news['title'] = re.sub('[^ \.\,\?\!a-zA-Z0-9\u3131-\u3163\uac00-\ud7a3]+', '', " ".join(str(title).split()))
+			news['href'] = re.sub('[^ \.\,\?\!a-zA-Z0-9\u3131-\u3163\uac00-\ud7a3]+', '', " ".join(str(href).split()))
+			news['body'] = re.sub('[^ \.\,\?\!a-zA-Z0-9\u3131-\u3163\uac00-\ud7a3]+', '', " ".join(str(body).split()))[55:]
 
 
 			newslist.append(news)
@@ -75,11 +84,15 @@ def word_count():
 	wordcount = {}
 	i_file=open("./crawl_output_merged.txt","r+", encoding='utf-8')
 	o_file=open("./word_count.txt","w", encoding='utf-8')
-	for word in i_file.read().split():
-		if word not in wordcount:
-			wordcount[word] = 1
-		else:
-			wordcount[word] += 1
+	kkma = Kkma()
+
+	for wordline in i_file:
+		words = kkma.nouns(wordline)
+		for word in words or []:
+			if word not in wordcount:
+				wordcount[word] = 1
+			else:
+				wordcount[word] += 1
 	i_file.close()
 
 	keys = list(wordcount.keys())
@@ -93,7 +106,7 @@ def word_count():
 def merge_files(file_count):
 	w_file=open("./crawl_output_merged.txt","w", encoding='utf-8')
 	idx = 1
-	while idx <= file_count:
+	for idx in range(file_count):
 		filename ="./crawl_output_%d.txt"%idx
 		r_file=open(filename,"r+", encoding='utf-8')
 		for lines in r_file.read():
@@ -136,13 +149,14 @@ def log(message):
 	sts = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 	print ("%s : %s"% (sts, message))
 
-log("start crawl..")
+
 
 if __name__ == '__main__':
 	result = Queue()
 
+	log("start crawl..")
 	jobs = []
-	for i in range(3):
+	for i in range(1):
 		p = Process(target=crawl, args=((datetime.datetime.now() - datetime.timedelta(days = i)).strftime('%Y%m%d'),i))
 		jobs.append(p)
 	for p in  jobs:
@@ -161,7 +175,7 @@ if __name__ == '__main__':
 	'''
 
 	log("merge..ing..")
-	merge_files(3)
+	merge_files(1)
 	log("counting words and saving that to mysql..")
 	word_count()
 	log("complete!")
